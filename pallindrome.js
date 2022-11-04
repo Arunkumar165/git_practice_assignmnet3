554518da4e5861e833658a823e5532cfe15d1b5d
ArunKumar165 <vk0799027@gmail.com>
2022-11-04 13:15:23 +0530

    checkcode

---
+++ pallindrome.js
@@ -0,0 +1,7 @@
+let str="naman"
+let flag=true
+for(ley i=0;i<=str.length-1;i++){
+if(str[i]=str[str.length-1]){
+flag=false;
+}
+}
---
+++ prime.js
@@ -0,0 +1,12 @@
+let num=10;
+let count=0
+for(i=2;i<=N;i++){
+if(num%i==0){
+count++
+}
+}if(count==1){
+console.log("num is prime")
+}
+else{
+console.log("num is not prime")
+}
