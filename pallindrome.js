91854a61b245b6e72f516653979e84a74080bcfb
ArunKumar165 <vk0799027@gmail.com>
2022-11-04 14:32:30 +0530

    codebyarun

--- pallindrome.js
+++ pallindrome.js
@@ -1,7 +1,15 @@
-let str="naman"
+ 
+let num="naman"
+function palin(str){
+
+
 let flag=true
 for(ley i=0;i<=str.length-1;i++){
-if(str[i]=str[str.length-1]){
+if(str[i]!=str[str.length-1]){
 flag=false;
 }
+
 }
+console.log(flag)
+}
+console.log(palin(num))
--- prime.js
+++ prime.js
@@ -1,12 +1,16 @@
 let num=10;
+function prime(N){
 let count=0
 for(i=2;i<=N;i++){
-if(num%i==0){
+if(N%i==0){
 count++
 }
-}if(count==1){
-console.log("num is prime")
+}if(count==2){
+console.log("is prime")
 }
 else{
-console.log("num is not prime")
+console.log("is not prime")
 }
+
+}
+prime(num)
